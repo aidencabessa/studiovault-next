b88cccc29fc89be186cac07f6ac4544d06ea70c3
aiden cabessa <97645706+aidencabessa@users.noreply.github.com>
2026-05-18 17:21:40 -0700

    Add files via upload

--- components/ScriptCard.js
+++ components/ScriptCard.js
@@ -1,4 +1,3 @@
-'use client'
 import { useState } from 'react'
 import Link from 'next/link'
 import { highlight } from '../lib/highlight'
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-'use client'
 import { useState } from 'react'
 import Head from 'next/head'
 import Nav from '../components/Nav'
